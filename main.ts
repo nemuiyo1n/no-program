8fbb7fd5ce56074354539e956d58177ab7bc6a4d
nemuiyo1n <takumeisabu@gmail.com>
2026-09-08 21:05:28 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,365 @@
+music.play(music.tonePlayable(1320, music.beat(BeatFraction.Half)), music.PlaybackMode.InBackground)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    # # . # #
+    . # # # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # # # #
+    # . # . #
+    # . # . #
+    `)
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . # # # .
+    . # . . .
+    . # # # .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . # # . .
+    . # . # .
+    . # . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    `)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    `)
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . # # .
+    . # . . #
+    . . . # .
+    . . # . .
+    . # # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # # # # #
+    # . . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # # # # .
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # # # # .
+    # . . . .
+    # # # # .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    # # # # #
+    # . . . #
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # # # # .
+    . . . # .
+    # # # # .
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # # # # .
+    # . . . .
+    # # # # .
+    `)
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    # . # . #
+    # # . # #
+    # . . . #
+    `)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    # # # # #
+    # . . . #
+    `)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # # # # .
+    . . . # .
+    # # # # .
+    `)
+basic.showLeds(`
+    # . . # .
+    # . . # .
+    # # # # .
+    # . . # .
+    # . . # .
+    `)
+basic.showLeds(`
+    # . . # .
+    # . . # .
+    # . . # .
+    # . . # .
+    . # # . .
+    `)
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    # # # . .
+    # . . # .
+    # . . # .
+    # . . # .
+    # # # . .
+    `)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    # . . # .
+    # . . # .
+    # . . # .
+    # . . # .
+    . # # . .
+    `)
+basic.showLeds(`
+    # . . . #
+    # # . . #
+    # . # . #
+    # . . # #
+    # . . . #
+    `)
+basic.pause(2000)
+music.play(music.tonePlayable(1320, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+basic.pause(100)
+music.play(music.tonePlayable(1320, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+basic.pause(100)
+music.play(music.tonePlayable(1320, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+for (let index = 0; index < 10; index++) {
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        # . . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        # # . . .
+        # . # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . # # #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        # # # # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # . # . #
+        # . . . #
+        # . . . #
+        `)
+    basic.pause(100)
+}
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # # # # .
+    . . . # .
+    # # # # .
+    `)
+basic.showLeds(`
+    # . . # .
+    # . . # .
+    . # # # .
+    # . . # .
+    # . . # .
+    `)
+basic.showLeds(`
+    # . . # .
+    # . . # .
+    # . . # .
+    # . . # .
+    . # # . .
+    `)
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    # # # . .
+    # . . # .
+    # . . # .
+    # . . # .
+    # # # . .
+    `)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    # . # . #
+    # # . # #
+    # . . . #
+    `)
+basic.showLeds(`
+    # . . . #
+    # # . . #
+    # . # . #
+    # . . # #
+    # . . . #
+    `)
+basic.clearScreen()
+basic.forever(function () {
+	
+})
