ff73fafdccf6fa77b844b00c2f694a9b6652bfb9
nemuiyo1n <takumeisabu@gmail.com>
2026-09-08 21:47:18 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -232,11 +232,11 @@ basic.showLeds(`
     # . . . #
     `)
 basic.pause(2000)
-music.play(music.tonePlayable(1320, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(1320, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
 basic.pause(100)
-music.play(music.tonePlayable(1320, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(1320, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
 basic.pause(100)
-music.play(music.tonePlayable(1320, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(1320, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
 for (let index = 0; index < 10; index++) {
     basic.showLeds(`
         # . . . #
